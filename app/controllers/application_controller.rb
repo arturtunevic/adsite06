19d05e53668e259e42b1d50cf75cce4187cb4878
Motiejus <motiejus.labzentis@gmail.com>
2019-12-02 05:30:05 +0200

    added login and ads logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+
+  def guest_user
+    if session[:guest_user_id] != nil
+      true
+    else
+      false
+    end
+  end
+
+  def create_guest_user
+    u = User.new(email: "guest_#{Time.now.to_i}#{rand(100)}@example.com")
+    u.save!(validate: false)
+    session[:guest_user_id] = u.id
+    redirect_to '/'
+  end
+
+  def delete_guest_session
+    u = User.find(session[:guest_user_id])
+    User.delete(u.id)
+    session[:guest_user_id] = nil
+    redirect_to root_path
+  end
+
 end
---
+++ db/migrate/20191202015500_create_ads.rb
@@ -0,0 +1,16 @@
+class CreateAds < ActiveRecord::Migration[6.0]
+  def change
+    create_table :ads do |t|
+      t.string :name
+      t.string :model
+      t.integer :mileage
+      t.integer :horsepower
+      t.string :fuel
+      t.string :gearbox
+      t.integer :price
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
